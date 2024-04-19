735c87770c2a0217dd49efac4c222092a20e5f8b
GrzegorzWirtek <grzegorz.wirtek@wp.pl>
2024-04-19 13:28:24 +0200

    add delete popup and handlers to it

---
+++ components/DeletePopup.tsx
@@ -0,0 +1,28 @@
+type DeletedItem = {
+	deletedItem: { productName: string; index: number };
+	deletePopup: (shouldDelete: boolean) => void;
+};
+
+export default function DeletePopup({ deletedItem, deletePopup }: DeletedItem) {
+	const { productName, index } = deletedItem;
+
+	return (
+		<div className='fixed top-0 left-0 w-full h-full flex justify-center items-center bg-red-600'>
+			<div className='flex flex-wrap justify-center items-center gap-4'>
+				<h3 className='basis-full text-center'>
+					Czy na pewno chcesz usunąć produkt <strong>{productName}</strong>?
+				</h3>
+				<button
+					className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+					onClick={() => deletePopup(false)}>
+					NIE
+				</button>
+				<button
+					className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+					onClick={() => deletePopup(true)}>
+					TAK
+				</button>
+			</div>
+		</div>
+	);
+}
--- components/DetailsListEdit.tsx
+++ components/DetailsListEdit.tsx
@@ -3,6 +3,12 @@
 import { useAppContext } from '@/app/context/AppContext';
 import tempdb from '@/tempdb/tempdb';
 import { useState } from 'react';
+import DeletePopup from './DeletePopup';
+
+type DeleteItem = {
+	productName: string;
+	index: number;
+};
 
 export default function DetailsListEdit() {
 	const { checkedArea } = useAppContext();
@@ -10,6 +16,8 @@ export default function DetailsListEdit() {
 	const prodArr = tempdb.filter((item) => item.id === checkedArea)[0].prodArr;
 
 	const [products, setProducts] = useState(prodArr);
+	const [deletePopupActive, setDeletePopupActive] = useState(false);
+	const [deletedItem, setDeletedItem] = useState<DeleteItem>({} as DeleteItem);
 
 	const handleChange = (
 		e: React.FormEvent<HTMLInputElement>,
@@ -32,63 +40,85 @@ export default function DetailsListEdit() {
 		console.log('Add new!');
 	};
 
-	const handleDelete = (productName: string) => {
-		console.log(`delete ${productName}`);
+	const handleDelete = (productName: string, index: number) => {
+		setDeletedItem({ productName, index });
+		setDeletePopupActive(true);
+	};
+
+	const deletePopup = (shouldDelete: boolean) => {
+		shouldDelete
+			? (setDeletePopupActive(false), deleteItem())
+			: (setDeletePopupActive(false), setDeletedItem({} as DeleteItem));
+	};
+
+	const deleteItem = () => {
+		console.log(
+			`Delete item ${deletedItem.productName} with index: ${deletedItem.index}`,
+		);
+		setDeletedItem({} as DeleteItem);
 	};
 
 	return (
-		<form
-			className='w-full flex flex-wrap justify-between'
-			onSubmit={handleSubmit}>
-			<button
-				type='button'
-				className='basis-[45%] bg-red-900 text-white p-2'
-				onClick={handleAddNew}>
-				DODAJ NOWY
-			</button>
-			<button type='submit' className='basis-[45%] bg-red-900 text-white py-2'>
-				ZATWIERDŹ ZMIANY
-			</button>
-			<table className='w-full mt-4  table-auto bg-red-400'>
-				<tbody>
-					{products.map((product, index) => (
-						<tr key={index} className=''>
-							<td className='border py-1 px-2 w-[70%]'>
-								<input
-									type='text'
-									value={product.name}
-									className='w-full'
-									onChange={(e) => handleChange(e, index, 'name')}
-								/>
-							</td>
-							<td className='border py-1 px-2 w-[15%]'>
-								<input
-									type='number'
-									value={product.amount}
-									className='w-full'
-									onChange={(e) => handleChange(e, index, 'amount')}
-								/>
-							</td>
-							<td className='border py-1 px-2 w-[15%] font-semibold text-red-900'>
-								<input
-									type='number'
-									value={product.level}
-									className='w-full font-semibold text-red-900'
-									onChange={(e) => handleChange(e, index, 'level')}
-								/>
-							</td>
-							<td className='border py-1 px-2 w-[15%] font-semibold text-red-900'>
-								<button
-									type='button'
-									className='font-bold px-2'
-									onClick={() => handleDelete(product.name)}>
-									X
-								</button>
-							</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-		</form>
+		<>
+			{deletePopupActive && (
+				<DeletePopup deletedItem={deletedItem} deletePopup={deletePopup} />
+			)}
+
+			<form
+				className='w-full flex flex-wrap justify-between'
+				onSubmit={handleSubmit}>
+				<button
+					type='button'
+					className='basis-[45%] bg-red-900 text-white p-2'
+					onClick={handleAddNew}>
+					DODAJ NOWY
+				</button>
+				<button
+					type='submit'
+					className='basis-[45%] bg-red-900 text-white py-2'>
+					ZATWIERDŹ ZMIANY
+				</button>
+				<table className='w-full mt-4  table-auto bg-red-400'>
+					<tbody>
+						{products.map((product, index) => (
+							<tr key={index} className=''>
+								<td className='border py-1 px-2 w-[70%]'>
+									<input
+										type='text'
+										value={product.name}
+										className='w-full'
+										onChange={(e) => handleChange(e, index, 'name')}
+									/>
+								</td>
+								<td className='border py-1 px-2 w-[15%]'>
+									<input
+										type='number'
+										value={product.amount}
+										className='w-full'
+										onChange={(e) => handleChange(e, index, 'amount')}
+									/>
+								</td>
+								<td className='border py-1 px-2 w-[15%] font-semibold text-red-900'>
+									<input
+										type='number'
+										value={product.level}
+										className='w-full font-semibold text-red-900'
+										onChange={(e) => handleChange(e, index, 'level')}
+									/>
+								</td>
+								<td className='border py-1 px-2 w-[15%] font-semibold text-red-900'>
+									<button
+										type='button'
+										className='font-bold px-2'
+										onClick={() => handleDelete(product.name, index)}>
+										X
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</form>
+		</>
 	);
 }
